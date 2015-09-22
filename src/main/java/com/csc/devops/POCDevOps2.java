20aaf9b1d843d84a5647ba1099b5f786111c99b0
EC2 Default User <ec2-user@ip-172-31-28-152.ap-southeast-1.compute.internal>
2015-09-22 07:01:12 +0000

    some change

--- src/main/java/com/csc/devops/POCDevOps2.java
+++ src/main/java/com/csc/devops/POCDevOps2.java
@@ -12,7 +12,7 @@ public class POCDevOps2 {
 	String username;
 	String firstName;
 	String lastName;
-	String emailID;	// add new commment 236
+	String emailID;	// add new commment 237
 	
 	private POCDevOps2(){		
 	}
