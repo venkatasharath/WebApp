e29772a862ed3a99c20764c3e494e4df4fd4883d
EC2 Default User <ec2-user@ip-172-31-28-152.ap-southeast-1.compute.internal>
2015-09-22 05:09:10 +0000

    new project

---
+++ src/main/java/com/csc/devops/POCDevOps2.java
@@ -0,0 +1,59 @@
+/**
+ * 
+ */
+package com.csc.devops;
+
+/**
+ * @author vyeshantarao
+ *
+ */
+public class POCDevOps2 {
+	
+	String username;
+	String firstName;
+	String lastName;
+	String emailID;	
+	
+	private POCDevOps2(){		
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public String getEmailID() {
+		return emailID;
+	}
+	
+	public void setEmailID(String emailID) {
+		this.emailID = emailID;
+	}
+	
+	public String toString(POCDevOps2 obj)
+	{
+		String name = obj.getFirstName();
+		name.toUpperCase();
+		
+		return name;
+	}
+}
